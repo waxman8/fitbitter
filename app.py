a030e89732915cb82d87bb17d1aabd2996b6a0ab
Martin de Villiers <martindevilliers@Martins-MacBook-Air.local>
2025-11-10 20:59:55 +0000

    back-end to run using the port env. variable , for GCP deployment

--- app.py
+++ app.py
@@ -481,4 +481,5 @@ def auth_status():
 if __name__ == "__main__":
     # This allows us to use a plain HTTP callback
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(debug=True, host="0.0.0.0", port=port)
